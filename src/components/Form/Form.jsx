89c1da7a21184dcbd1991a08057e46aac098321a
Maksym Morenko <morenko.maxim@gmail.com>
2022-06-14 04:38:25 +0300

    валидация формы инпут firstName и Email

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -21,6 +21,24 @@ class Form extends Component {
         // [event.target.name] строка будет прилетать динамически, по указаному имени
         this.setState({[event.target.name]: event.target.value})
     }
+    // валидация формы на момент когда поле больше не активно
+    // создадим метод валидейтНеим
+    validateName = () => {
+        // будем проверять firstName на количество символов
+        if (this.state.firstName.length < 5) {
+            alert('Your first name can\'t be less then 5 letters' )
+        }
+    }
+    // повесим эту проверку на обработчик события on blur
+    // Событие blur вызывается когда элемент теряет фокус
+    validateEmail = () => {
+        console.log(this.state.email, /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.state.email));
+        if (!(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.state.email))) {
+            alert('Email is not valid')
+        }
+    }
+
+
     render() {
         //сделаем деструктуризацию стейта чтобы им удобнее было полльзоваться
         const {firstName, email} = this.state;
@@ -33,6 +51,7 @@ class Form extends Component {
                     // value будет брать значение из нашего стейта
                     value={firstName}
                     onChange={this.handleChange}
+                    onBlur={this.validateName}
                 />
                 {/* теперь инпут контролируемый и мы можем взять данные из стейта и прверить их (валидация) */}
                 <input 
@@ -42,6 +61,7 @@ class Form extends Component {
                     // value будет брать значение из нашего стейта
                     value={email}
                     onChange={this.handleChange}
+                    onBlur={this.validateEmail}
                 />
             </div>
         );
